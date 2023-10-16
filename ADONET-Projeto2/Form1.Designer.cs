528c033331f76bb761e67fb58cf80d7df51c00ee
Prof. Matioli <matioli@unicamp.br>
2023-10-16 00:06:08 -0300

    Implementadas algumas funcionalidades do DataGridView

--- ADONET-Projeto2/Form1.Designer.cs
+++ ADONET-Projeto2/Form1.Designer.cs
@@ -29,34 +29,231 @@ namespace ADONET_Projeto2
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rdMasc = new System.Windows.Forms.RadioButton();
+            this.rdFem = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.rdOutro = new System.Windows.Forms.RadioButton();
+            this.grpDados = new System.Windows.Forms.GroupBox();
             this.dgvAlunos = new System.Windows.Forms.DataGridView();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.grpDados.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(105, 19);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(381, 29);
+            this.txtNome.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "E-mail:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(525, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sexo:";
+            // 
+            // rdMasc
+            // 
+            this.rdMasc.AutoSize = true;
+            this.rdMasc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdMasc.Location = new System.Drawing.Point(605, 17);
+            this.rdMasc.Name = "rdMasc";
+            this.rdMasc.Size = new System.Drawing.Size(78, 28);
+            this.rdMasc.TabIndex = 6;
+            this.rdMasc.TabStop = true;
+            this.rdMasc.Text = "Masc.";
+            this.rdMasc.UseVisualStyleBackColor = true;
+            // 
+            // rdFem
+            // 
+            this.rdFem.AutoSize = true;
+            this.rdFem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdFem.Location = new System.Drawing.Point(703, 17);
+            this.rdFem.Name = "rdFem";
+            this.rdFem.Size = new System.Drawing.Size(72, 28);
+            this.rdFem.TabIndex = 6;
+            this.rdFem.TabStop = true;
+            this.rdFem.Text = "Fem.";
+            this.rdFem.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(525, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Celular:";
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCelular.Location = new System.Drawing.Point(605, 62);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(267, 29);
+            this.txtCelular.TabIndex = 8;
+            // 
+            // rdOutro
+            // 
+            this.rdOutro.AutoSize = true;
+            this.rdOutro.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdOutro.Location = new System.Drawing.Point(799, 17);
+            this.rdOutro.Name = "rdOutro";
+            this.rdOutro.Size = new System.Drawing.Size(75, 28);
+            this.rdOutro.TabIndex = 6;
+            this.rdOutro.TabStop = true;
+            this.rdOutro.Text = "Outro";
+            this.rdOutro.UseVisualStyleBackColor = true;
+            // 
+            // grpDados
+            // 
+            this.grpDados.Controls.Add(this.txtEmail);
+            this.grpDados.Controls.Add(this.txtNome);
+            this.grpDados.Controls.Add(this.rdFem);
+            this.grpDados.Controls.Add(this.label1);
+            this.grpDados.Controls.Add(this.rdMasc);
+            this.grpDados.Controls.Add(this.label2);
+            this.grpDados.Controls.Add(this.rdOutro);
+            this.grpDados.Controls.Add(this.txtCelular);
+            this.grpDados.Controls.Add(this.label3);
+            this.grpDados.Controls.Add(this.label4);
+            this.grpDados.Location = new System.Drawing.Point(8, 12);
+            this.grpDados.Name = "grpDados";
+            this.grpDados.Size = new System.Drawing.Size(878, 109);
+            this.grpDados.TabIndex = 9;
+            this.grpDados.TabStop = false;
             // 
             // dgvAlunos
             // 
             this.dgvAlunos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAlunos.Location = new System.Drawing.Point(12, 179);
+            this.dgvAlunos.Location = new System.Drawing.Point(8, 128);
             this.dgvAlunos.Name = "dgvAlunos";
-            this.dgvAlunos.Size = new System.Drawing.Size(878, 211);
-            this.dgvAlunos.TabIndex = 0;
+            this.dgvAlunos.Size = new System.Drawing.Size(878, 303);
+            this.dgvAlunos.TabIndex = 10;
+            this.dgvAlunos.SelectionChanged += new System.EventHandler(this.dgvAlunos_SelectionChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(105, 65);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(381, 29);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovo.Location = new System.Drawing.Point(8, 447);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(124, 39);
+            this.btnNovo.TabIndex = 11;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(156, 447);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(124, 39);
+            this.btnSalvar.TabIndex = 11;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(305, 447);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(124, 39);
+            this.btnRefresh.TabIndex = 11;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(454, 447);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(124, 39);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(898, 498);
+            this.ClientSize = new System.Drawing.Size(894, 498);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnNovo);
             this.Controls.Add(this.dgvAlunos);
+            this.Controls.Add(this.grpDados);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Cadastro de Alunos";
+            this.grpDados.ResumeLayout(false);
+            this.grpDados.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rdMasc;
+        private System.Windows.Forms.RadioButton rdFem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.RadioButton rdOutro;
+        private System.Windows.Forms.GroupBox grpDados;
         private System.Windows.Forms.DataGridView dgvAlunos;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnCancelar;
     }
 }
 
--- ADONET-Projeto2/Form1.cs
+++ ADONET-Projeto2/Form1.cs
@@ -14,60 +14,110 @@ namespace ADONET_Projeto2
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = null;
+        SqlDataAdapter daAluno;
+        DataTable dtAluno;
+        SqlCommandBuilder builder;
+
         Font f = new Font("Arial", 14);
 
         public Form1()
         {
 
             InitializeComponent();
+            // Criando a conexão  
+
             carregaDataGrid();
 
+            //Configura o DataGridView
             dgvAlunos.Font = f;
-            dgvAlunos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvAlunos.SelectionMode = DataGridViewSelectionMode.FullRowSelect; //seleciona a linha inteira
+            dgvAlunos.MultiSelect = false; //impede seleção de várias células ou linhas
 
             dgvAlunos.Columns[0].Visible = false;
             dgvAlunos.Columns[1].Visible = false;
-            dgvAlunos.Columns[2].DataPropertyName = "nome";
-            dgvAlunos.Columns[3].DataPropertyName = "email";
-            dgvAlunos.Columns[4].DataPropertyName = "sexo";
-            dgvAlunos.Columns[5].DataPropertyName = "celular";
+
+            dgvAlunos.Columns[0].DataPropertyName = "idaluno";
+            dgvAlunos.Columns[1].DataPropertyName = "idcurso";
+            dgvAlunos.Columns[2].DataPropertyName = "Nome";
+            dgvAlunos.Columns[3].DataPropertyName = "Email";
+            dgvAlunos.Columns[4].DataPropertyName = "Sexo";
+            dgvAlunos.Columns[5].DataPropertyName = "Celular";
+
             dgvAlunos.Columns[2].HeaderText = "Nome";
             dgvAlunos.Columns[2].Width = 300;
             dgvAlunos.Columns[3].HeaderText = "E-mail";
             dgvAlunos.Columns[3].Width = 300;
-            dgvAlunos.Columns[4].HeaderText = "SEXO";
+            dgvAlunos.Columns[4].HeaderText = "Sexo";
             dgvAlunos.Columns[4].Width = 60;
             dgvAlunos.Columns[5].HeaderText = "Celular";
             dgvAlunos.Columns[5].Width = 150;
 
+            //bloqueio acesso aos campos do formulário. 
+            //só poderá acessar quando for inserir um novo ou editar
+            grpDados.Enabled = false;
 
         }
 
         private void carregaDataGrid()
         {
-            SqlConnection con = null;
-            try
+             try
             {
-                // Criando a conexão  
                 string ConString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
                 con = new SqlConnection(ConString);
-                con.Open();
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
                 //cria a instância da classe SqlDataAdapter
-                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM aluno", con);
+                daAluno = new SqlDataAdapter("SELECT idaluno,idcurso,Nome,Email,Sexo,Celular FROM Aluno", con);
                 //Cria uma instância de Data Table
-                DataTable dt = new DataTable();
+                dtAluno = new DataTable();
                 //Preenche o DataTable com os dados vindos do Data Source, atavés do Data Adapter
-                da.Fill(dt);
-                dgvAlunos.DataSource = dt;
+                int v = daAluno.Fill(dtAluno);
+
+                builder = new SqlCommandBuilder(daAluno);
+                daAluno.InsertCommand = builder.GetInsertCommand();
+                daAluno.UpdateCommand = builder.GetUpdateCommand();
+                daAluno.DeleteCommand = builder.GetDeleteCommand();
+
+                dgvAlunos.DataSource = dtAluno;  //vincula o DataGriView com a Fonte de Dados
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 MessageBox.Show("Falha ao acessar o banco de dados!", "Aviso do sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            finally
-            {   // Fechando a conexão  
-                con.Close();
+ 
+        }
+
+        private void dgvAlunos_SelectionChanged(object sender, EventArgs e)
+        {
+            DataGridView row = (DataGridView)sender;
+            if (row.CurrentRow == null)
+                return;
+
+            //preenche os Textboxes com os valores da linha selecionada no DataGrid
+            txtNome.Text = dgvAlunos.CurrentRow.Cells[2].Value.ToString();
+            txtEmail.Text = dgvAlunos.CurrentRow.Cells[3].Value.ToString();
+            txtCelular.Text = dgvAlunos.CurrentRow.Cells[5].Value.ToString();
+
+            string sx = dgvAlunos.CurrentRow.Cells[4].Value.ToString();
+            if (sx.Equals("M")) rdMasc.Checked = true;
+            else if (sx.Equals("F")) rdFem.Checked = true;
+            else rdOutro.Checked = true;
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (daAluno == null)
+                throw new Exception("Parametro adapter está null");
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+
+            if(daAluno.Update(dtAluno)>0)
+            {
+                MessageBox.Show("Banco de Dados atualizado com sucesso!", "Aviso do sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+
         }
     }
 
